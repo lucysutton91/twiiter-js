38e279ed65214ff7fa33cda28dcb5868c07a262e
Pat <patpetillo@gmail.com>
2017-09-19 11:50:55 -0400

    error free!!!

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require( 'express' );
+const nunjucks = require('nunjucks');
 const app = express();
+const routes = require('./routes');
 
+let locals = {
+    title: 'Twitter Template',
+    people: [
+        {name: 'Pat'},
+        {name: 'Lucy'}
+    ]
+};
 
-app.use(function (request, response, next) {
-    console.log(request);
-    next();
-})
-
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function (err, output) {
+    if (err) throw err;
+    console.log(output);
+});
 
 app.listen(3000, function () {
     console.log('server listening')
 });
+
+app.use('/', routes);
+app.use(express.static('public'))
+
